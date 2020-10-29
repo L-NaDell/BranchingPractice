ceb15bd323f51e022dcb8f953bf85f40bed9be20
Lindsay <lindsaynadell89@gmail.com>
2020-10-29 10:47:07 -0400

    Kanzi thoughts

--- BranchingPractice/Program.cs
+++ BranchingPractice/Program.cs
@@ -6,7 +6,9 @@ namespace BranchingPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("FOOOOOOD!~~K");
+            Console.WriteLine("I don't think she sees the food. I will remind her it is there so she gives it to me.~~K");
+            Console.WriteLine("Maybe if I slowly climb onto her lap....~~K");
         }
     }
 }

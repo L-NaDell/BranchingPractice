ad982db1121a8087a6af156e471d66e5b2cd8b8d
Lindsay <lindsaynadell89@gmail.com>
2020-10-29 10:40:52 -0400

    my eatting habits

--- BranchingPractice/Program.cs
+++ BranchingPractice/Program.cs
@@ -6,7 +6,10 @@ namespace BranchingPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Dinner!");
+            Console.WriteLine("This is hot, I will let it cool off.");
+            Console.WriteLine("I think it's ready. Time to eat my dinner.");
+            Console.WriteLine("That was great!");
         }
     }
 }
